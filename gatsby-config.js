bc51ab7bdc50884e9eb4444b8269205968560058
Lindsey Rampton <lindseyrampton@gmail.com>
2021-10-14 22:08:24 -0400

    :tada: first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,26 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Bookmarks",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-sanity",
-      options: {
-        projectId: "06h855sl",
-        dataset: "",
-      },
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "Bookmarks",
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+    plugins: [{
+            resolve: "gatsby-source-sanity",
+            options: {
+                projectId: "06h855sl",
+                dataset: "production",
+            },
+        },
+        "gatsby-plugin-sass",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
